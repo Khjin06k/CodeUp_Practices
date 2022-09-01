1aed66fabd6b6107fffc84d6c4863b5d7bf54017
김희진 <khj4209kk@gmail.com>
2022-09-01 22:19:21 +0900

    1036~1037 알고리즘 문제

---
+++ src/Ex1036.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ex1036 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char value = sc.nextLine().charAt(0);
+        int result = (int) value;
+
+        System.out.println(result);
+    }
+}
---
+++ src/Ex1037.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Ex1037 {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int value = sc.nextInt();
+            System.out.println((char) value);
+        }
+}
+
