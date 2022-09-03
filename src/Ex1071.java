4f85325a781a4021a529c306455d16041c86ac36
김희진 <khj4209kk@gmail.com>
2022-09-03 17:51:38 +0900

    알고리즘 문제

--- src/Ex1071.java
+++ src/Ex1071.java
@@ -1,20 +1,29 @@
 import java.util.Scanner;
-//연속 입력임을 인식 (중간에 0이 있더라도 계속 입력 및 0을 인식한 순간 break 진행이 필요함)
 public class Ex1071 {
-    public static void main(String[] args){
+    /*public static void main(String[] args){
+
         Function();
     }
 
-    public static void Function(){
+    public static void Function() {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        if (a != 0) {
+            System.out.println(a);
+            Function();
+        }
+    }*/
+    //추후 재귀함수를 이용하여 문제를 해결할 수 있는지 생각 및 확인 필요
 
-        switch (a) {
-            case 0:
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            int a = sc.nextInt();
+            if(a==0){
                 break;
-            default:
-                System.out.println(a);
-                Function();
+            }
+            System.out.println(a);
         }
     }
 }
